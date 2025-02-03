27e2e2f13389bc9b71e2e4f4d88356410efc0bde
Cole Morgan <colemmorgann@gmail.com>
2025-02-03 01:42:04 -0500

    Updated spcb thumbnail

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -52,7 +52,7 @@ export default function Projects() {
       <div className="mt-6 md:grid grid-cols-5 gap-8">
         <figure className="col-span-3">
           <img
-            src="/projects/spcb2.webp"
+            src="/projects/spcb_new.webp"
             alt=""
             className="w-full rounded-sm"
           />
